8eaab942db6b838ffd36f874b3ff2e73d32f3dc6
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-05-25 10:24:41 -0500

    se realiza ejemplo de como recibir el body en post request

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,17 +1,36 @@
 import { Request, RequestHandler, Response } from 'express';
+import { User } from '../models/user';
 
 
 export const crearUsuario = (req: Request, resp: Response) => {
+
+    const { email, name, password } : User = req.body;
+
+    if(name.length < 5){
+        return resp.status(400).json({
+            ok: false,
+            message: 'El nombre debe ser de 5 letras'
+        })
+    }
+
     return resp.json({
-        ok: true
+        ok: true,
+        email,
+        name,
+        password
     })
 }
 
 
 export const loginUsuario = (req: Request, resp: Response) => {
+
+    const { email, password } : User = req.body;
+
     return resp.json({
         ok: true,
-        message: 'Login'
+        message: 'Login',
+        email,
+        password
     })
 }
 
