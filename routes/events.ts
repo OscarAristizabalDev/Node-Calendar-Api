4dd77f716908a4a3f2d83a954e4cbab5f4fa2aa7
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-05-28 17:57:33 -0500

    Se agrega middlaware para validar el token en las rutas de eventos

--- routes/events.ts
+++ routes/events.ts
@@ -5,12 +5,14 @@
 import { Router } from 'express';
 import { check } from 'express-validator'; // check, middleware encarga de validar un campo en particular
 import { ActualizarEvento, CrearEvento, EliminarEvento, getEventos } from '../controllers/events';
+import { validarJWT } from '../middlewares/validarJwt';
+
 const router = Router();
 
-router.get('/', getEventos);
-router.post('/create', CrearEvento);
-router.put('/update', ActualizarEvento);
-router.delete('/delete', EliminarEvento);
+router.get('/', validarJWT, getEventos);
+router.post('/create', validarJWT, CrearEvento);
+router.put('/update', validarJWT, ActualizarEvento);
+router.delete('/delete', validarJWT, EliminarEvento);
 
 // Se exportar todos los router
 module.exports = router;
